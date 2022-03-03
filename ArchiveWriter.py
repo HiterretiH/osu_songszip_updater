f97f3bbeef2b5a757993dcf56510405dc8010a16
HiterretiH <47186981+hiterretih@users.noreply.github.com>
2022-03-03 16:45:21 +0300

    Implement ArchiveWriter methods

--- ArchiveWriter.py
+++ ArchiveWriter.py
@@ -1,2 +1,23 @@
+from threading import Thread
+from Progress import ListProgress
+import zipfile
+import os
+
+
 class ArchiveWriter:
-    pass
+    def create_archive(self, filename: str, path: str) -> ListProgress:
+        counter = [0, 0]
+        progress = ListProgress(counter)
+        Thread(target=self.__create_archive_thread, args=(filename, path, counter)).start()
+
+        return progress
+
+    def __create_archive_thread(self, filename: str, path: str, counter: list[int]):
+        with zipfile.ZipFile(file=filename, mode="w", compression=zipfile.ZIP_DEFLATED, allowZip64=True) as f:
+            for root, dirs, files in os.walk(path):
+                counter[1] += len(files)
+            for root, dirs, files in os.walk(path):
+                dir_path = os.path.relpath(root, path)
+                for file in files:
+                    f.write(os.path.join(root, file), os.path.join(dir_path, file))
+                    counter[0] += 1
